938086a9b16984eb59f36f65d7f72b6855b35e2b
Falcroft7 <kevin.patry22@gmail.com>
2025-12-12 15:37:19 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,11 +1,6 @@
-export let listePersos = [];
-export let listePatrons = [];
-export let listeBancs = [];
-export let listeClasses = [];
-export let listeRoles = [];
-export let currentPage = 1;
-
-export const ITEMS_PER_PAGE = 30;
+// ===================== CONSTANTES =====================
+export const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRd-Z9L00gE2wnlhgD1LSlSyNDo2dej3UcX_-UtnN2NBrV8tzHxib_Mxx28d3k9CoWjzhQUxcQjK7TA/pub?output=csv";
+export const CSV_URL_PATRONS = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRd-Z9L00gE2wnlhgD1LSlSyNDo2dej3UcX_-UtnN2NBrV8tzHxib_Mxx28d3k9CoWjzhQUxcQjK7TA/pub?gid=1864789699&single=true&output=csv";
 
 export const sectionsFiche = [
   { title: "Général", fields: ["Banc", "Origine"] },
@@ -14,3 +9,14 @@ export const sectionsFiche = [
   { title: "Caractéristiques", fields: ["Force", "Dextérité", "Constitution", "Intelligence", "Sagesse", "Charisme", "Total"] },
   { title: "Attaque", fields: ["Type Attaque", "Type Ultime"] },
 ];
+
+// ===================== ÉTAT GLOBAL =====================
+export let listePersos = [];
+export let listePatrons = [];
+export let listeBancs = [];
+export let listeClasses = [];
+export let listeRoles = [];
+
+// ===================== PAGINATION =====================
+export let currentPage = 1;
+export const ITEMS_PER_PAGE = 30;
