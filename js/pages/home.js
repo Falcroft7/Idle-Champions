f92f3515d6dd7408818ada5a33c254ed523935e3
Falcroft7 <kevin.patry22@gmail.com>
2025-12-12 15:47:19 +0100

    Update home.js

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,27 +1,27 @@
-import { renderPage } from "../utils.js";
-import { affichListe } from "./liste.js";
-import { affichAffiliationList } from "./affiliation.js";
-import { affichPatronsList } from "./patrons.js";
+import { affichListe, affichAffiliationList, affichPatronsList } from "./liste.js";
 
 export function affichHome() {
-  renderPage(`
+  const app = document.getElementById("app");
+  app.innerHTML = `
     <div class="home-container">
-      <img src="Banner/dnd-banner.png" class="home-banner">
+      <img src="Banner/dnd-banner.png" alt="D&D Banner" class="home-banner">
       <h1>D&D - Idle Champions</h1>
       <p class="home-intro">
         Salutations, héros en herbe !<br>
-        Préparez vos champions pour la gloire !
+        Gérez vos champions, découvrez leurs patrons mystérieux et préparez des combos épiques.<br>
+        Appuyez sur un bouton pour plonger dans l’aventure !
       </p>
-
       <div class="home-buttons">
         <button id="btn-champions">Champions</button>
         <button id="btn-affiliations">Affiliations</button>
         <button id="btn-patrons">Patrons</button>
+        <button id="btn-synergies">Synergies</button>
       </div>
     </div>
-  `);
+  `;
 
-  document.getElementById("btn-champions").onclick = affichListe;
-  document.getElementById("btn-affiliations").onclick = affichAffiliationList;
-  document.getElementById("btn-patrons").onclick = affichPatronsList;
+  document.getElementById("btn-champions").addEventListener("click", affichListe);
+  document.getElementById("btn-affiliations").addEventListener("click", affichAffiliationList);
+  document.getElementById("btn-patrons").addEventListener("click", affichPatronsList);
+  document.getElementById("btn-synergies").addEventListener("click", () => alert("En développement"));
 }
