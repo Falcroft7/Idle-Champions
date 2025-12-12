6574cb2b90258d52d8561d3cc0f98108f856f2d8
Falcroft7 <kevin.patry22@gmail.com>
2025-12-12 15:30:02 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+import { affichHome } from "./pages/home.js";
+import { listePersos, listePatrons, listeBancs, listeClasses, listeRoles } from "./state.js";
+import { normalizeString } from "./utils.js";
 
+Papa.parse(CSV_URL, {
+  download: true,
+  header: true,
+  complete(results) {
+    // Ton code merge CSV ici
+    // Puis :
+    affichHome();
+  }
+});
+
+Papa.parse(CSV_URL_PATRONS, {
+  download: true,
+  header: true,
+  complete(results) {
+    listePatrons = results.data.filter(p => p.Nom && p.Nom.trim());
+  }
+});
